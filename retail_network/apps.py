f63789b64d949c3689871d075943edc297a2df73
alexandr.lobanov <alexandp83@mail.ru>
2023-11-26 13:27:33 +0300

    Сделан вывод в админ-панели созданных объектов

--- retail_network/apps.py
+++ retail_network/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RetailNetworkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'retail_network'
+    verbose_name = "Розничная сеть"  # переименовали отображение наименования приложения

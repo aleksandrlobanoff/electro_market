01924cb87733279471da247f9bb8ff8c8a53e8ee
alexandr.lobanov <alexandp83@mail.ru>
2023-11-26 13:59:45 +0300

    !

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-%rbz+$xl+&xfrus=c6114bzuj$7ieiy(a^@c-hr9=sh8^s2ac2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
